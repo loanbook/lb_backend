60c46260183c66ac5b43780efed94e9acda061c9
M Asif Saleem <asif@caansoft.com>
2019-03-04 17:53:43 +0500

    Fixed routes

---
+++ routes/apis/admin/index.js
@@ -0,0 +1,17 @@
+const usersRouter = require('../admin/users');
+const investorRouters = require('../admin/investors');
+const borrowerRouters = require('../admin/borrowers');
+const loansRouters = require('../admin/loans');
+const installmentRouter = require('../admin/installments');
+
+const ADMIN_BASE_PATH = 'admin/';
+
+module.exports = (app, basePath,  passport) => {
+
+	app.use(basePath + ADMIN_BASE_PATH + 'users', passport.authenticate('jwt', {session: false}), usersRouter);
+	app.use(basePath + ADMIN_BASE_PATH + 'investors', passport.authenticate('jwt', {session: false}), investorRouters);
+	app.use(basePath + ADMIN_BASE_PATH + 'borrowers', passport.authenticate('jwt', {session: false}), borrowerRouters);
+	app.use(basePath + ADMIN_BASE_PATH + 'loans', passport.authenticate('jwt', {session: false}), loansRouters);
+	app.use(basePath + ADMIN_BASE_PATH + 'installments', passport.authenticate('jwt', {session: false}), installmentRouter);
+
+};
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,11 +1,7 @@
 const passport = require('passport');
 
+const adminUrl = require('./admin');
 const authRouter = require('./auth');
-const usersRouter = require('./admin/users');
-const investorRouters = require('./admin/investors');
-const borrowerRouters = require('./admin/borrowers');
-const loansRouters = require('./admin/loans');
-const installmentRouter = require('./admin/installments');
 
 
 const ROOT_BASE = "/api/v1/";
@@ -18,12 +14,10 @@ module.exports = (app) => {
 	// Passport Middleware
 	app.use(passport.initialize());
 
-
 	app.use(ROOT_BASE + 'auth', authRouter);
-	app.use(ROOT_BASE + 'admin/users', passport.authenticate('jwt', {session: false}), usersRouter);
-	app.use(ROOT_BASE + 'admin/investors', passport.authenticate('jwt', {session: false}), investorRouters);
-	app.use(ROOT_BASE + 'admin/borrowers', passport.authenticate('jwt', {session: false}), borrowerRouters);
-	app.use(ROOT_BASE + 'admin/loans', passport.authenticate('jwt', {session: false}), loansRouters);
-	app.use(ROOT_BASE + 'admin/installments', passport.authenticate('jwt', {session: false}), installmentRouter);
+
+	// Apis admin routes.
+
+	adminUrl(app, ROOT_BASE,  passport);
 
 };
