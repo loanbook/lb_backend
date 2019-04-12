bed0f4e2a94137724ba84fb890503cc5702a4a02
max's <junaidahmed501@gmail.com>
2019-04-12 19:46:31 +0500

    transactions get api

--- controllers/apis/admin/transactionsController.js
+++ controllers/apis/admin/transactionsController.js
@@ -2,9 +2,7 @@ const models = require('../../../models');
 
 
 exports.transactionsGet = async function (req, res, next) {
-    models.Transaction.findAll({
-        limit: 2
-    }).then(trans => {
+    models.Transaction.findAll({}).then(trans => {
         res.status(200).json({ data: trans });
     }).catch(error => {
         res.status(500).json({ message: error.message });
